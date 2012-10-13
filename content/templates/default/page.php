d5705697e7e81708bd021e741ea5266a49ed8e29
ahaic <g>
2012-10-14 04:58:14 +1030

    Revert "update template"
    This reverts commit 61d0e3f36e39f3491ff49658818394c3a8d3fd0b.

--- content/templates/default/page.php
+++ content/templates/default/page.php
@@ -5,32 +5,13 @@
 if(!defined('EMLOG_ROOT')) {exit('error!');} 
 ?>
 <div id="content">
-
-
 <div id="contentleft">
-
-
-
-
 	<h2><?php echo $log_title; ?></h2>
 	<?php echo $log_content; ?>
-	<p class="att"><?php blog_att($logid); ?></p>
-	
-	
- 	<?php blog_comments($comments); ?>    
-	
-	
+	<?php blog_comments($comments); ?>
 	<?php blog_comments_post($logid,$ckname,$ckmail,$ckurl,$verifyCode,$allow_remark); ?>
-	
-	 
-	
 	<div style="clear:both;"></div>
-	
-	
-</div>           <!--end #contentleft-->
-
-
-
+</div><!--end #contentleft-->
 <?php
  include View::getView('side');
  include View::getView('footer');
