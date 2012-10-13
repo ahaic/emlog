39cc53be4ab6f08d517ca59df02102f444927df0
ahaic <g>
2012-10-14 04:25:41 +1030

    database problem again
    prefix  tyre_

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ define('DB_PASSWD','google');
 //database name
 define('DB_NAME','tyre');
 //database prefix
-define('DB_PREFIX','emlog_');
+define('DB_PREFIX','tyre_');
 //auth key
 define('AUTH_KEY','bmapfKlyST5jNkSW7*kgZiYxH83cLkQS5fdacb5d6301f52551f1d8418e1ff14d');
 //cookie name
